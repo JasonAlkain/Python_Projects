3c0409bb6a982c8a1ee75e4c55953159a8ff1812
Jason <AarsZach@yahoo.com>
2021-07-15 16:53:11 -0600

    Create interClasses.py

---
+++ InterClasses/interClasses.py
@@ -0,0 +1,19 @@
+
+
+class Player():
+    name = ''
+    race = ''
+    gender = ''
+
+
+class Gameplay(Player):
+    score = 0
+    items = []
+    gameMap = {}
+
+
+player = Player
+
+gameplay = Gameplay
+
+gameplay.name
