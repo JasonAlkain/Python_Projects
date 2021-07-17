91247a80b5ad518ef01c462b0151f1ff4610cbc5
Jason <AarsZach@yahoo.com>
2021-07-16 19:05:36 -0600

    Update interClasses.py

--- InterClasses/interClasses.py
+++ InterClasses/interClasses.py
@@ -25,7 +25,7 @@ class Player(Character):
         super().__init__(name, race, gender)
         self.skills = skills
         self.money = money
-        self.itmes = items
+        self.items = items
         self.equipment = equipment
 
     # return the info of this class
@@ -37,20 +37,20 @@ class Player(Character):
         if self.skills != None:
             msg += 'Skills:\n'
             for skill in self.skills:
-                msg += '| {}\n'.format(skill)
+                msg += '\t{}\n'.format(skill)
 
         # print the players money
         # TODO: Add a type check here
         msg += 'Money: {}\n'.format(self.money)
 
         # check if there were any items passed to the class
-        if self.itmes != None:
+        if self.items != None:
             msg += 'Items:\n'
             for item in self.items:
-                msg += '| {}\n'.format(item)
+                msg += '\t{}\n'.format(item)
 
         # check if there was any equipment passed to the class
-        if equipment != None:
+        if self.equipment != None:
             msg += 'Equipment:\n'
             for equip in self.equipment:
                 msg += '\t{}: {}\n'.format(equip, self.equipment[equip])
@@ -76,29 +76,42 @@ class NPC(Character):
         if self.chatText != None:
             msg += 'Chat:\n'
             for chat in self.chatText:
-                msg += '| {}\n'.format(chat)
+                msg += '\t{}\n'.format(chat)
 
         # check if there was any equipment passed to the class
         if self.quests != None:
             msg += 'quests:\n'
             for quest in self.quests:
-                msg += '| {}\n'.format(quest)
+                msg += '\t{}\n'.format(quest)
 
         msg += '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n'
         return msg
 
 
-if __name__ == "__main__":
-    equipment = {'Head': None, 'Body': 'Shirt', 'Legs': 'Pants'}
-
+def newPlayer():
+    # request player for first and last name
     fname = input('What is your first name: ').capitalize()
     lname = input('What is your last name: ').capitalize()
+    # add first and last name together to make one name
     name = '{} {}'.format(fname, lname)
-
+    # request player for  race
     race = input('What is your race: ').capitalize()
+    # request player for gender
     gender = input('What is your gender: ').capitalize()
+    # Setup skills
+    skills = ['Magic Missile', 'Friends (Concentration)', 'Mage Hand']
+    # Setup money
+    money = 50
+    # Setup items
+    items = ['Potion of healing', 'Smart Phone']
+    # create basic equipment
+    equipment = {'Head': None, 'Body': 'Shirt', 'Legs': 'Pants'}
+    # return the completed player object
+    return Player(name, race, gender, skills, money, items, equipment)
 
-    player_1 = Player(name, race, gender, None, 0, None, equipment)
+
+if __name__ == "__main__":
+    player_1 = newPlayer()
     print(player_1.info())
 
     npc = NPC('Jake', 'Human', 'Male', ["I'm Jake from State Farm."], None)
